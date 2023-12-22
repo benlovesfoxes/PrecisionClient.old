33e8324257520d272f81be3a6b976c0e63eab5f6
benlovesfoxes <97691798+benlovesfoxes@users.noreply.github.com>
2023-12-22 09:37:10 -0500

    Create playercontroller.cs

---
+++ web/playercontroller.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical) * moveSpeed;
+
+        rb.velocity = movement;
+    }
+}
